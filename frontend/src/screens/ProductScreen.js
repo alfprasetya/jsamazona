bd292266049b9b68ecd2fd5fb64449108bcd99a3
Prasetya <a.pr4setya@gmail.com>
2025-06-15 23:41:58 +0700

    add routing and error handling with 404 screen; implement product screen and utility for URL parsing

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,10 @@
+import { parseRequestUrl } from '../utils.js';
+
+const ProductScreen = {
+    render: () => {
+        const { id } = parseRequestUrl();
+        return `<div> Product Screen ${id} </div>`;
+    }
+}
+
+export default ProductScreen;
